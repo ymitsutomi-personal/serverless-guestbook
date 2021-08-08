7c3ca12d84e7a4ce33760f4a16ee92fb0f5e98bc
ymitsutomi-personal <79044843+ymitsutomi-personal@users.noreply.github.com>
2021-08-08 18:34:23 +0900

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://7e8315e9.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
